efa83e3a37bda2aa20f1c8656dc8f55a75e01fca
Rasrizal <rasrizalrosdi96@gmail.com>
2022-01-08 15:27:22 +0800

    updated new api from shopee

--- shopeeTools.py
+++ shopeeTools.py
@@ -3,6 +3,22 @@ import json
 import browser_cookie3
 import os
 import time
+import itertools
+import threading
+import time
+import sys
+
+def animatedLoading():
+    chars = "/—\|" 
+    for char in chars:
+        sys.stdout.write('\r'+'Calculating...'+char)
+        time.sleep(.1)
+        sys.stdout.flush() 
+
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
 def getHeader(url,browserOption):
   cookiesString = ""
   result = {'error':0,'headersDict':""}
@@ -22,19 +38,23 @@ def getHeader(url,browserOption):
     elif browserOption == 6:
       cookies = list(browser_cookie3.chromium(domain_name='shopee.com.my'))
   except:
-    print("Please select installed browser or try again")
+    print("Error> Cannot use selected browser. Try another browser")
     result['error'] = 1
     result['headersDict'] = ""
     return result
-
+  # dont want this cookies 
+  rejectCookies = ["AMP_TOKEN","G_AUTHUSER_H","G_ENABLED_IDPS"]
   # check for cookies length
   cookiesLength = len(cookies)
   if(cookiesLength<0):
-    print("Error > Please make sure the browser installed or make sure that you logged in to shopee.com.my and wait for a few seconds try again")
+    print("Error > (Cookies)")
     exit()
   
   for i in range(len(cookies)):
-        cookiesString+= cookies[i].name+"="+cookies[i].value+"; "
+    if not cookies[i].name in rejectCookies:
+      cookiesString+= cookies[i].name+"="+cookies[i].value+"; "
+  # print("cookiesString below")      
+  # print(cookiesString)
   #process header here .. set the header 
   headersDict['authority'] = 'shopee.com.my'
   headersDict['method'] = 'GET'
@@ -64,9 +84,11 @@ def getHeader(url,browserOption):
 def processResponseData(dataDict):
   #check for error .. 
   if dataDict['error'] == 19:
-    print("Error > Please make sure the browser installed or make sure that you logged in to shopee.com.my and wait for a few seconds try again")
+    cls()
+    print("Error > Problem with browser or the login. Hurm not sure ..")
     return [0,-1]
   elif dataDict['error'] != 0:
+    cls()
     print("Error > Unknown error")
     return [0,-1]
   # processedData ['totalPaid','offset']
@@ -82,11 +104,11 @@ def processResponseData(dataDict):
     return [itemPrice,-1]
 
   #process for the items details 
-
-  if len(dataDict['data']['details_list']) == 0:
+  # print(dataDict)
+  if len(dataDict['data']['order_data']['details_list']) == 0:
     return [itemPrice,-1]
   else:
-    itemDetailsDict = dataDict['data']['details_list']
+    itemDetailsDict = dataDict['data']['order_data']['details_list']
     
   for i in range(len(itemDetailsDict)):
     if itemDetailsDict[i]['status']['status_label']['text'] == 'label_order_completed':
@@ -98,10 +120,9 @@ def processResponseData(dataDict):
 
   return [totalItemPrice,offset]
 
-
 def print_menu():
   menu_options = {
-    1: 'Chrome',
+    1: 'Chrome (not sure if it works now)',
     2: 'Firefox',
     3: 'Edge',
     4: 'Opera',
@@ -118,6 +139,7 @@ def print_menu():
   return
 
 def runProcess(optionSelected):
+  cls()
   browserOption = optionSelected
   responseList = {}
   totalCalculated = 0
@@ -125,14 +147,15 @@ def runProcess(optionSelected):
   processedData = {}
   totalCalculatedPrice = 0
   extraStr = ""
-  print("Calculating the damage from shopee")
+  # print("Calculating the damage from shopee")
   payload={}
   loop = 1
   print("Process started..")
+  
   while nextOffset >= 0:
     if loop == 1:
-      print("Silo tunggu.. it might take a few seconds")
-    url = "https://shopee.com.my/api/v4/order/get_order_list?limit=5&list_type=3&offset="+str(nextOffset)
+      print("Silo tunggu.. just a few seconds")
+    url = "https://shopee.com.my/api/v4/order/get_all_order_and_checkout_list?limit=5&offset="+str(nextOffset)
     headers = getHeader(url,browserOption)['headersDict']
     response = requests.request("GET", url, headers=headers, data=payload)
     responseList = response.json()
@@ -144,34 +167,76 @@ def runProcess(optionSelected):
     # im scared shopee detect we requesting to much so i should limit to at least 1 sec
     if loop > 20:
       time.sleep(1)
+  
+  return totalCalculated
 
-  if totalCalculated > 0:
-    if totalCalculated > 1000000 and totalCalculated < 2000000:
-      extraStr = "Beli gapo banyok tu"
-    elif totalCalculated > 2000000:
-      extraStr = "banyak weh pitih. tolong bank in ko ambo"
-    elif totalCalculated > 100000 and totalCalculated < 1000000:
-      extraStr = "Okay la tu"
-    elif totalCalculated < 100000:
-      extraStr = "ANDA ADALAH INSAN TERPILIH. Pandai berjimat"
-    totalCalculatedPrice = str(totalCalculated)[0:-2] +"."+ str(totalCalculated)[-2:]
-    print('Total Spent = RM ' + str(totalCalculatedPrice))
-    print(extraStr)
-    os.system("pause")
-  else:
-    print('Total Spent = RM 0.00')
-    print(extraStr)
-    os.system("pause")
-  return
+def testFunction(optionSelected):
+  time.sleep(2)
+  i = 0
+  while i == 2:
+    i = i + 1
+
+  return 100.00
+
+class ThreadWithReturnValue(threading.Thread):
+    def __init__(self, group=None, target=None, name=None, args=(), kwargs=None, *, daemon=None):
+        threading.Thread.__init__(self, group, target, name, args, kwargs, daemon=daemon)
+
+        self._return = None
+
+    def run(self):
+        if self._target is not None:
+            self._return = self._target(*self._args, **self._kwargs)
+
+    def join(self):
+        threading.Thread.join(self)
+        return self._return
+    
+    def is_alive(self):
+        return threading.Thread.is_alive(self)
 
 def main():
   optionArray = [1,2,3,4,5,6]
+  totalCalculated = 0;
   while(True):
+    cls()
     print_menu()
-    option = int(input('Select the browser used: '))
+    
+    try : 
+      option = int(input('Select the browser used: '))
+    except ValueError:
+       print("Not a number! Try again.")
+       continue
+    
+
+
     if option in optionArray:
       browseroption = option
-      runProcess(browseroption)
+      
+      # define the thread ..
+      threadProcess = ThreadWithReturnValue(name='runProcess', target=runProcess, args=(browseroption,))
+      threadProcess.start()
+      
+      # check if thread still running .. if not then exit and get the result 
+      while threadProcess.is_alive():
+        animatedLoading()
+      cls()
+
+      totalCalculated = threadProcess.join()
+      
+      if totalCalculated > 1000000 and totalCalculated < 2000000:
+        extraStr = "Beli gapo banyok tu"
+      elif totalCalculated > 2000000:
+        extraStr = "banyak weh pitih. tolong bank in ko ambo"
+      elif totalCalculated > 100000 and totalCalculated < 1000000:
+        extraStr = "Okay la tu"
+      elif totalCalculated < 100000:
+        extraStr = "ANDA ADALAH INSAN TERPILIH. Pandai berjimat"
+      totalCalculatedPrice = str(totalCalculated)[0:-2] +"."+ str(totalCalculated)[-2:]
+      print('Total Spent = RM ' + str(totalCalculatedPrice))
+      print(extraStr)
+      os.system("pause")
+
     elif option == 0:
         print('Thanks for using my simple program :D')
         os.system("pause")
@@ -179,6 +244,6 @@ def main():
     else:
         print('Invalid option. Please enter a number between 1 and 6.')
     os.system('cls' if os.name == 'nt' else 'clear')
+
 if __name__ == "__main__":
     main()
-
